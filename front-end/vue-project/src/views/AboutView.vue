a3a82526035629b255998f4dd14a1dda04444c52
SUNG-CHE LIN <86179263+Isongzhe@users.noreply.github.com>
2024-05-07 01:08:07 +0800

    new v1
    完全打掉重練用js開發
    如果你想要，也可以回到上個版本

---
+++ front-end/vue-project/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <InputComponent />
+  </div>
+</template>
+<script setup>
+import InputComponent from '../components/Input.vue'
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
